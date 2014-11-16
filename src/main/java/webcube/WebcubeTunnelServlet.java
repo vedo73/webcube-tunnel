7896446a3bad07a31bf24b15f2cf0973e80c8444
kowabungappl@gmail.com <kowabungappl@gmail.com>
2014-11-16 00:43:15 -0500

    initial commit

---
+++ src/main/java/webcube/WebcubeTunnelServlet.java
@@ -0,0 +1,50 @@
+package webcube; /**
+ * Created by satyakb on 11/15/14.
+ */
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.glyptodon.guacamole.GuacamoleException;
+import org.glyptodon.guacamole.net.GuacamoleSocket;
+import org.glyptodon.guacamole.net.GuacamoleTunnel;
+import org.glyptodon.guacamole.net.InetGuacamoleSocket;
+import org.glyptodon.guacamole.protocol.ConfiguredGuacamoleSocket;
+import org.glyptodon.guacamole.protocol.GuacamoleConfiguration;
+import org.glyptodon.guacamole.servlet.GuacamoleHTTPTunnelServlet;
+import org.glyptodon.guacamole.servlet.GuacamoleSession;
+
+public class WebcubeTunnelServlet
+        extends GuacamoleHTTPTunnelServlet {
+
+    @Override
+    protected GuacamoleTunnel doConnect(HttpServletRequest request)
+            throws GuacamoleException {
+
+        System.out.println("wtf");
+        // Create our configuration
+        GuacamoleConfiguration config = new GuacamoleConfiguration();
+        config.setProtocol("vnc");
+        config.setParameter("hostname", "localhost");
+        config.setParameter("port", "5901");
+        config.setParameter("password", "potato");
+
+        // Connect to guacd - everything is hard-coded here.
+        GuacamoleSocket socket = new ConfiguredGuacamoleSocket(
+                new InetGuacamoleSocket("localhost", 4822),
+                config
+        );
+
+        // Establish the tunnel using the connected socket
+        GuacamoleTunnel tunnel = new GuacamoleTunnel(socket);
+
+        // Attach tunnel to session
+        HttpSession httpSession = request.getSession(true);
+        GuacamoleSession session = new GuacamoleSession(httpSession);
+        session.attachTunnel(tunnel);
+
+        // Return pre-attached tunnel
+        return tunnel;
+
+    }
+
+}
